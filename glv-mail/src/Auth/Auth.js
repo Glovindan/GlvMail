0c7b45b4518551c201af33e2ba8ef482c040f377
Glovindan Golovkin <turbo69nice@gmail.com>
2022-02-11 01:26:03 +0400

    [write[ code

--- glv-mail/src/Auth/Auth.js
+++ glv-mail/src/Auth/Auth.js
@@ -1,18 +1,57 @@
-import {loadAPI, signIn} from './AuthLogic';
+import {isSignedIn, loadAPI, signIn, signOut} from './AuthLogic';
 import React from "react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button} from "react-bootstrap";
 
 class Auth extends React.Component {
-    componentDidMount() {
-        loadAPI();
+    constructor(props) {
+        super(props);
+
+        this.handleLogInClick = this.handleLogInClick.bind(this);
+        this.handleLogOutClick = this.handleLogOutClick.bind(this);
+        this.state = {isLoggedIn: false};
+    }
+
+    async componentDidMount() {
+        loadAPI().then(
+            (res) => {
+                console.log(res);
+                this.setState({isLoggedIn: isSignedIn()})
+            },
+            (err) => console.log(err)
+        );
+    }
+
+
+    handleLogInClick() {
+        signIn().then(user => {
+            console.log(user.getBasicProfile().getId());
+            this.setState({isLoggedIn: isSignedIn()})
+        })
+    }
+
+    handleLogOutClick() {
+        signOut().then(str => {
+            console.log(str);
+            this.setState({isLoggedIn: isSignedIn()})
+        })
     }
 
     render() {
+        const loginStatus = this.state.isLoggedIn;
+
+        let button;
+        if(loginStatus) {
+            button = <Button variant="outline-primary" onClick={this.handleLogOutClick}>Sign out</Button>
+        } else {
+            button = <Button variant="outline-dark" onClick={this.handleLogInClick}>Sign in with Google</Button>
+        }
+
         return(
             <div>
-                <Button variant="outline-dark" onClick={signIn}>Sign in with Google</Button>
+                {button}
+                {/*<Button variant="outline-dark" onClick={this.handleLogInClick}>Sign in with Google</Button>*/}
                 {/*<Button variant="outline-primary" onClick={signOut}>Sign out</Button>*/}
                 {/*<Button variant="outline-primary" onClick={isSignedIn}>Check</Button>*/}
             </div>
--- glv-mail/src/Auth/AuthLogic.js
+++ glv-mail/src/Auth/AuthLogic.js
@@ -1,64 +1,44 @@
 const CLIENT_ID = "462028362119-ps2hcrcj7qst93kd7rgdbdjubbppuv5l.apps.googleusercontent.com";
 const SCOPES = 'https://mail.google.com';
+const GAPI = window.gapi;
 
-const initAPI = () => {
-    window.gapi.client.init( {
-        'client_id': CLIENT_ID,
-        'scope': SCOPES
-    }).then(
-        () => {
-            console.log("OK");
-            if(isSignedIn()) {
-                redirect()
-            }
-        },
-        (error) => console.log(error)
-    )
-}
-
-const loadAPI = () => {
-    window.gapi.load('client', initAPI);
-}
-
-function signIn() {
-    const googleAuth = window.gapi.auth2.getAuthInstance();
-
-    googleAuth.signIn().then(user => {
-        console.log("signed in");
-        console.log(user.getBasicProfile().getId());
-        if(isSignedIn()) {
-            window.gapi.client.load("https://gmail.googleapis.com/$discovery/rest?version=v1").then(() => {
-                const userId = user.getBasicProfile().getId(); //TODO:УДАЛИТЬ ОТСЮДА ИЛИ ЗАПИСАТЬ В ЛОКАЛСТОРАДЖ МБ
-                let messageId = '';
-                window.gapi.client.gmail.users.messages.list({userId: userId}).then(someVar => {
-                    messageId = someVar.result.messages[0].id;
-                    console.log(messageId)
-                    window.gapi.client.gmail.users.messages.get({userId: userId, id: messageId}).then(message => {
-                        console.log(message);
-                    })
-                })
-                redirect();
-            })
-            //init GMAIL API
-            //Maybe save
-        }
+const loadAPI = async () => {
+    //1. Загрузить библиотеки gapi.client и gapi.auth2
+    //2. Инициализировать библиотеку gapi.client
+    //3. ???
+    //4. PROFIT
+    return new Promise((res, rej) => {
+        GAPI.load('client:auth2', () => {
+            GAPI.client.init( {
+                'client_id': CLIENT_ID,
+                'scope': SCOPES
+            }).then(
+                () => {
+                    res("OK");
+                },
+                (err) => rej(err)
+            )
+        });
     });
 }
 
-function signOut() {
-    const googleAuth = window.gapi.auth2.getAuthInstance();
-    googleAuth.signOut().then(user => {
-        console.log("signed out");
-    });
+const signIn = async () => {
+    const googleAuth = GAPI.auth2.getAuthInstance();
+    return new Promise((res) => {
+        googleAuth.signIn().then(res);
+    })
 }
 
-function redirect() {
-    console.log("BOOM REDIRECTED")
+const signOut = async() => {
+    const googleAuth = GAPI.auth2.getAuthInstance();
+    return new Promise(res => {
+        googleAuth.signOut().then(() => res("signed out"));
+    })
 }
 
 const isSignedIn = () => {
-    console.log(window.gapi.auth2.getAuthInstance().isSignedIn.get());
-    return window.gapi.auth2.getAuthInstance().isSignedIn.get();
+    console.log(GAPI.auth2.getAuthInstance().isSignedIn.get());
+    return  GAPI.auth2.getAuthInstance().isSignedIn.get()
 }
 
-export {loadAPI, signIn, signOut, isSignedIn, redirect}
+export {loadAPI, signIn, signOut, isSignedIn}
